74329d22d2ada9c3da1571e92e8281dc8b57a497
Matias <cuentademierda1234567890@gmail.com>
2022-11-12 19:03:08 -0400

    no nombrar si tiene ya numeracion arreglado

--- Enumerar todos los archivos.bat
+++ Enumerar todos los archivos.bat
@@ -4,12 +4,12 @@ set n=0
 
 for %%i in (*) do (
 
-	if not "%%~nxi" == "cambiar nombre.bat" (
-		set /a n+=1
-			if not "%%~nxi" == !n! (
-				ren "%%~nxi" !n!.nsp
-				)
+if not "%%~nxi" == "Enumerar todos los archivos.bat" (
+	set /a n+=1
+		if not "%%~nxi" == !n! (
+			ren "%%~nxi" !n!.nsp
+			)
 	)
 )
-echo Archivos en total en esta carpeta: %n%
+echo %n%
 pause
